f85da1da3bececaf73c09d201e2791535128d7e1
MD. Owasiul Islam <sanisuper0@gmail.com>
2025-08-28 22:41:36 +0600

    adding clear, fix call feature and alert of copy

--- script.js
+++ script.js
@@ -12,27 +12,55 @@ heart.forEach((btn) => {
 const copyBtns = document.querySelectorAll(".copyBtn");
 copyBtns.forEach((btn) => {
   btn.addEventListener("click", function () {
+    const card = btn.closest(".card");
+    const callNum = card.querySelector(".callNum").innerText;
     const copyCounts = document.getElementById("copy");
+    alert(`number is copied: ${callNum}`)
     let count = parseInt(copyCounts.innerText);
     count += 1;
     copyCounts.innerText = count;
   });
 });
 // call
+const callHistory = [];
 const callBtns = document.querySelectorAll(".callBtn");
 callBtns.forEach((btn) => {
   btn.addEventListener("click", function () {
     const coin = document.getElementById("coin");
     let coinConvert = parseInt(coin.innerText);
     const card = btn.closest(".card");
-const callId = card.querySelector(".callId").innerText;
-const callNum = card.querySelector(".callNum").innerText;
+    const callId = card.querySelector(".callId").innerText;
+    const callNum = card.querySelector(".callNum").innerText;
+    const callHistoryContainer = document.getElementById("history_container");
+    const date = new Date().toLocaleTimeString();
 
     if (coinConvert <= 0) {
       alert(`আপনার পর্যাপ্ত ব্যালেন্স নেই, ন্যূনতম ২০ কয়েন প্রয়োজন`);
     } else {
-      alert(`${callId}.. ${callNum}... কল হচ্ছে`);
+      alert(`কল হচ্ছে.. ${callId}.. ${callNum}.. `);
       coin.innerText = coinConvert - 20;
+      const div = document.createElement("div");
+      div.innerHTML = `
+          <div
+                class="rounded-lg p-4 mt-5 flex flex-row items-center justify-between bg-[#FAFAFA]"
+              >
+                <div class="astitle">
+                  <h3>${callId}</h3>
+                  <h4>${callNum}</h4>
+                </div>
+                 <span> ${date} </span>
+              </div>
+          `;
+      callHistory.push = {
+        id: callId,
+        num: callNum,
+        time: date,
+      };
+      callHistoryContainer.appendChild(div);
     }
   });
 });
+// clear
+document.getElementById('clear').addEventListener('click', function(){
+   return document.getElementById('history_container').innerHTML = ''
+})
